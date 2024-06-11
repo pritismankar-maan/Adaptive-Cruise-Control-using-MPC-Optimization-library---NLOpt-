e59a03aa98d9cbdccb7ff4b1167fb786e9a8954b
Ubuntu Linux <cris333.03@gmail.com>
2024-06-10 22:50:41 -0700

    “intial_edit”

---
+++ CMakeFiles/acc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/acc.dir/main.cpp.o"
+  "acc"
+  "acc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/acc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
